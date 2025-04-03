e05043914d03f6b02c7056150a7df36820b7a774
JonLloydDev <jon@jonlloyd.dev>
2025-04-02 22:57:42 -0400

    Player and movement set up

--- main.py
+++ main.py
@@ -5,20 +5,27 @@ import pygame
 
 # Local
 from constants import *
-
+from circleshape import CircleShape
+from player import Player
 
 def main():
     print('Starting Asteroids!')
     pygame.init()
+
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill('black')
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
-    print(f'Screen width: {SCREEN_WIDTH}')
-    print(f'Screen height: {SCREEN_HEIGHT}')
+        dt = clock.tick(60) / 1000
 
 if __name__ == '__main__':
     main()
